091afac74c0d267877ed63747fded7cd81358779
Vitcharm <mrxuweic@yahoo.com>
2022-09-04 22:52:34 +0800

    [Practice]creep role系统V1.0,抽出行为函数使结构清晰;

--- role.builder.js
+++ role.builder.js
@@ -2,31 +2,36 @@ var roleBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        console.log('builder working')
         if(creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
             creep.memory.building = false;
-            creep.say('🔄 harvest');
         }
         if(!creep.memory.building && creep.store.getFreeCapacity() === 0) {
             creep.memory.building = true;
-            creep.say('🚧 build');
         }
 
         if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
+            creep.say('🚧 build');
+            this.building(creep);
         }
         else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            creep.say('💰 harvest');
+            this.harvest(creep);
+        }
+    },
+    harvest: function(creep) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+        }
+    },
+    building: function(creep) {
+        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if(targets.length) {
+            if(creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
-    }
+    },
 };
 
 module.exports = roleBuilder;
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,19 +1,28 @@
+'use strict';
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+        if (creep.store.getFreeCapacity() > 0) {
+            creep.say('💰 harvest');
+            this.harvest(creep);
+        } else {
+            this.upgrading(creep);
+            creep.say('🔝 upgrad');
         }
-        else {
-            if(creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
+    },
+    harvest: function(creep) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
         }
-    }
+    },
+    upgrading: function(creep) {
+        if (creep.upgradeController(creep.room.controller) ===
+            ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller);
+        }
+    },
 };
 
 module.exports = roleUpgrader;
