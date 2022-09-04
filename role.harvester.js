864c39cd01201786a13023ad0dbefed32e7ece1c
Vitcharm <mrxuweic@yahoo.com>
2022-09-05 00:13:17 +0800

    [Practice]CreepRole系统V1.0,更新Harvester存放container;

--- role.harvester.js
+++ role.harvester.js
@@ -19,20 +19,31 @@ var roleHarvester = {
         }
     },
 
+    // 待优化提取地点查找部分
     deliver: function(creep) {
         var targets = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType === STRUCTURE_EXTENSION ||
-                        structure.structureType === STRUCTURE_SPAWN) &&
+                        structure.structureType === STRUCTURE_SPAWN ||
+                        structure.structureType === STRUCTURE_CONTAINER) &&
                     structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
             },
         });
         if (targets.length > 0) {
+            console.log(targets[0]);
             if (creep.transfer(targets[0], RESOURCE_ENERGY) ===
                 ERR_NOT_IN_RANGE) {
                 creep.moveTo(targets[0],
                     {visualizePathStyle: {stroke: '#ffffff'}});
             }
+        } else {
+            // 回基地待命
+            var tarSpawn = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType === STRUCTURE_SPAWN;
+                },
+            });
+            creep.moveTo(tarSpawn[0]);
         }
     },
 };
