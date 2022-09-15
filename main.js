0157cdffa1e89305a93be022d356528aff327623
Vitcharm <mrxuweic@yahoo.com>
2022-09-16 00:32:40 +0800

    [E44N43]数量控制系统优化代码结构，优化主程序结构;

--- main.js
+++ main.js
@@ -5,33 +5,10 @@ require('creepApi');
 
 module.exports.loop = function() {
     /** Creep number manager system v2.0 **/
-    if (!Memory.spawnList) baseSpawn.initSpawnTask(RoleTypeMap);
-    for (const curName in Memory.creeps) {
-        if (!Game.creeps[curName]) {
-            console.log(`${curName} has come to its end, deleting memory...`);
-            delete Memory.creeps[curName];
-        }
-    }
-    if (Memory.spawnList.length === 0) {
-        console.log('Check new spawn config...');
-        RoleTypeMap.forEach(function(typeValue) {
-            let creepsInType = _.filter(Game.creeps,
-                (creep) => creep.memory.configName === typeValue.configName);
-            if (creepsInType.length < typeValue.size) {
-                let updatedSize = baseSpawn.addSpawnTask(typeValue);
-                console.log(
-                    `spawn list size updated: ${updatedSize},
-                     configName:${typeValue.configName},
-                     now size ${creepsInType.length},
-                     target size ${typeValue.size}`);
-            }
-        });
-    }
-
-    baseSpawn.checkSpawnTask();
-    // baseSpawn.vizSpawning();
+    baseSpawn.runSpawnManager();
 
     /** Creep role work system v2.0 **/
+    console.log("!!!running role manager!!!")
     Object.values(Game.creeps).forEach(creep => creep.creepWork());
 };
 
--- prototype.spawn.js
+++ prototype.spawn.js
@@ -7,6 +7,15 @@ module.exports = function() {
 // 自定义的 Spawn 的拓展
 const SpawnExtension = {
 
+    runSpawnManager() {
+        console.log('!!!running spawn manager!!!');
+        if (!Memory.spawnList) baseSpawn.initSpawnTask(RoleTypeMap);
+        baseSpawn.checkCreepSpawnList();
+        let processRet = baseSpawn.processSpawnTask();
+        console.log(`process Spawn Task rst:${processRet}`);
+        baseSpawn.vizSpawning();
+    },
+
     initSpawnTask(roleMap) {
         console.log('Init Spawn Task');
         Memory.spawnList = [];
@@ -19,12 +28,33 @@ const SpawnExtension = {
         console.log('Init done, spawnList size:', Memory.spawnList.length);
     },
 
-    checkSpawnTask() {
-        if (Memory.spawnList.length === 0) {
-            console.log('spawn list length: ' + Memory.spawnList.length);
-            return;
+    checkCreepSpawnList() {
+        let isSomeOneDead = false;
+        for (const curName in Memory.creeps) {
+            if (!Game.creeps[curName]) {
+                console.log(
+                    `${curName} has come to its end, deleting memory...`);
+                delete Memory.creeps[curName];
+                isSomeOneDead = true;
+            }
         }
-        const spawnOk = this.mainSpawn(Memory.spawnList[0]);
+        if (Memory.spawnList.length === 0 || isSomeOneDead === true) {
+            console.log(
+                `Check new spawn config...length [${Memory.spawnList.length}], someone dead?${isSomeOneDead}`);
+            RoleTypeMap.forEach(function(typeValue) {
+                let creepsInType = _.filter(Game.creeps,
+                    (creep) => creep.memory.configName ===
+                        typeValue.configName);
+                if (creepsInType.length < typeValue.size) {
+                    let updatedSize = baseSpawn.addSpawnTask(typeValue);
+                    console.log(
+                        `spawn list size updated: ${updatedSize},
+                     configName:${typeValue.configName},
+                     now size ${creepsInType.length},
+                     target size ${typeValue.size}`);
+                }
+            });
+        }
     },
 
     addSpawnTask(roleType) {
@@ -32,7 +62,12 @@ const SpawnExtension = {
         return Memory.spawnList.length;
     },
 
-    mainSpawn(roleType) {
+    processSpawnTask() {
+        if (Memory.spawnList.length === 0) {
+            console.log('spawn list length: ' + Memory.spawnList.length);
+            return -10;
+        }
+        let roleType = Memory.spawnList[0];
         if (!Memory.creepNameCounter) Memory.creepNameCounter = 0;
         let newName = roleType.role + Memory.creepNameCounter;
         let mockRet = this.spawnCreep(roleType.body, newName,
@@ -43,28 +78,21 @@ const SpawnExtension = {
             if (mockRet === ERR_NAME_EXISTS) Memory.creepNameCounter++;
             return mockRet;
         }
+        // spawn creep here
         let spawnRet = this.spawnCreep(roleType.body, newName,
             {memory: {role: roleType.role, configName: roleType.configName}});
-
         // update spawnList
-        console.log(`spawn success, now size: ${Memory.spawnList.length}`);
         Memory.spawnList.shift();
-        console.log(`update list, now size: ${Memory.spawnList.length}`);
-
         // update creep config
-        creepApi.add(roleType.configName, roleType.role,
-            roleType.targetSite);
-        console.log(`Spawning new creep: ${roleType.role} ${roleType.configName}`);
-
+        creepApi.add(roleType.configName, roleType.role, roleType.targetSite);
         // update counter
         Memory.creepNameCounter = Memory.creepNameCounter + 1;
-        console.log(`now counter is ${Memory.creepNameCounter}`);
 
         return spawnRet;
     },
 
     vizSpawning() {
-        if (this.spawning) {
+        if (this.spawning !== null) {
             var spawningCreep = Game.creeps[this.spawning.name];
             this.room.visual.text(
                 '🛠️' + spawningCreep.memory.role,
