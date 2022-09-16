398952c2413a0b08f05ce32f665cdff959e1dc4c
Vitcharm <mrxuweic@yahoo.com>
2022-09-17 02:44:04 +0800

    [E44N43]优化repairer逻辑;

--- role.carrier.js
+++ role.carrier.js
@@ -14,23 +14,24 @@ module.exports = sourceId => ({
     performDuty: creep => {
         var targets = creep.room.find(FIND_MY_STRUCTURES, {
             filter: (structure) => {
-                return (structure.structureType === STRUCTURE_EXTENSION)
-                    || (structure.structureType === STRUCTURE_SPAWN)
+                return (structure.structureType === STRUCTURE_SPAWN)
+                    || (structure.structureType === STRUCTURE_EXTENSION)
                     || (structure.structureType === STRUCTURE_TOWER)
                     && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
             },
         });
         creep.say('🔄 deliver');
+        targets.sort(
+            (a, b) => b.store.getFreeCapacity(RESOURCE_ENERGY) - a.store.getFreeCapacity(RESOURCE_ENERGY));
         if (targets.length > 0) {
-            for (let i in targets) {
-                var transRet = creep.transfer(targets[i], RESOURCE_ENERGY);
-                if (transRet === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[i],
-                        {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+            console.log(targets[0].store.getFreeCapacity(RESOURCE_ENERGY));
+            console.log(targets[0]);
+            var transRet = creep.transfer(targets[0], RESOURCE_ENERGY);
+            if (transRet === ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0],
+                    {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
-        return (creep.store[RESOURCE_ENERGY] <= 0 || transRet === OK ||
-            transRet === ERR_FULL);
+        return (creep.store[RESOURCE_ENERGY] <= 0 || transRet === OK);
     },
 });
